7054be7483bec9a06076e4cd65e522c724641ed3
KerlyVal <182720458+kerlyval@users.noreply.github.com>
2024-12-10 14:35:44 -0500

    new-changelogo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ className }) => {
 	return (
 		<img
-			src="https://www.flaticon.com/de/kostenloses-icon/hotel_9922103"
+			src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fde%2Fkostenloses-icon%2Fhotel_9922103&psig=AOvVaw3MCeYCFuuxw96eH-RPiz6f&ust=1733944131573000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLDVn5n4nYoDFQAAAAAdAAAAABAS"
 			alt="Logo"
 			className={`size-14 ${className} hover:filter hover:brightness-110 hover:hue-rotate-90 hover:animate-rotate-180`}
 		/>
