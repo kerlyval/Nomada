c47514c23834f6f6958dc52b0b22bed60fbdf63c
KerlyVal <182720458+kerlyval@users.noreply.github.com>
2024-12-10 14:33:10 -0500

    new

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ className }) => {
 	return (
 		<img
-			src="../../icono-hotel.png"
+			src="https://www.flaticon.com/de/kostenloses-icon/hotel_9922103"
 			alt="Logo"
 			className={`size-14 ${className} hover:filter hover:brightness-110 hover:hue-rotate-90 hover:animate-rotate-180`}
 		/>
