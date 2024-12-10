66cc4081c92eb8b948c591a7644ada5aa260b059
KerlyVal <182720458+kerlyval@users.noreply.github.com>
2024-12-10 14:22:05 -0500

    new-logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ className }) => {
 	return (
 		<img
-			src="../../public/icono-hotel.png"
+			src="../assets/icono hotel.png"
 			alt="Logo"
 			className={`size-14 ${className} hover:filter hover:brightness-110 hover:hue-rotate-90 hover:animate-rotate-180`}
 		/>
